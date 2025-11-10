ba4c98d1447bd7477bce14f4fa121020834008f7
noctisnyxx <dinar.rahmat5@gmail.com>
2025-11-10 20:42:58 +0700

    Initial Commit

---
+++ chatting-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatting-app
+//
+//  Created by Dinar Hadiyanto on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatting-app/chatting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatting_appApp.swift
+//  chatting-app
+//
+//  Created by Dinar Hadiyanto on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct chatting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
