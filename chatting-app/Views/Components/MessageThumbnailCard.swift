6ed458535cfe10e99a9bccfdfe830588dd4ef1bb
noctisnyxx <dinar.rahmat5@gmail.com>
2026-01-07 06:52:47 +0700

    feat: in progressing with chatroom and refactoring

---
+++ chatting-app/Views/Components/MessageThumbnailCard.swift
@@ -0,0 +1,52 @@
+struct MessageThumbnailCard: View{
+    let msgMeta: MessageCover
+    var avatarURL = "https://avatar.iran.liara.run/public"
+    var body: some View{
+        NavigationLink{
+            ChatRoomView()
+        }label: {
+            cardContent
+        }
+    }
+    private var cardContent: some View{
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.gray.opacity(0))
+            .frame(height: 80)
+            .overlay(
+            HStack(){
+                avatarBox
+                messageBox
+            }.padding(.horizontal, 7)
+        )
+    }
+    private var avatarBox : some View{
+        AsyncImage(url: URL(string: avatarURL)
+        ){ image in
+            image.resizable()
+        } placeholder: {
+            Color.gray.opacity(0.3)
+        }
+            .frame(width: 70, height: 70)
+            .clipShape(
+                Circle()
+            )
+    }
+    private var messageBox: some View{
+        RoundedRectangle(cornerRadius: 10)
+            .fill(.gray.opacity(0.2))
+            .frame(height: 60)
+            .overlay(
+                VStack(
+                    alignment: .leading,
+                ){
+                    Text(msgMeta.name)
+                        .foregroundColor(.black)
+                        .font(.headline)
+                    Text(msgMeta.lastMessage)
+                        .foregroundColor(.gray)
+                        .font(.system(.caption, design: .rounded))
+                }.padding(5)
+                    .frame(maxWidth: .infinity, alignment: .leading
+            )
+    )}
+}
